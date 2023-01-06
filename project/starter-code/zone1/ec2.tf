7adf3648172b571953da00d7910ad9808ecd24a8
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2023-01-07 00:47:17 +0100

    IP changed

--- project/starter-code/zone1/ec2.tf
+++ project/starter-code/zone1/ec2.tf
@@ -3,7 +3,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-04ff923e070e28b56"
+   aws_ami            = "ami-03d757dc089d5fc0d"
    private_subnet_ids = module.vpc.private_subnet_ids
    public_subnet_ids  = module.vpc.public_subnet_ids
    vpc_id             = module.vpc.vpc_id
